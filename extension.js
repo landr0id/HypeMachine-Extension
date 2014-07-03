7180c6f2872bfe709fe6aac97a3afb9c3ad45569
Lander Brandt <landerbrandt@gmail.com>
2014-07-03 16:00:06 -0700

    Update domain

--- extension.js
+++ extension.js
@@ -5,82 +5,80 @@ var stylesheetUrl = chrome.extension.getURL("hypestyles.css");
 // This is all the JS that will be injected in the document body
 var main = function() {
 
-	//Random String generator.
-	var generator = function()
-	{
-		var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
-		var string_length = 8;
-		var randomstring = "";
-		for (var i=0; i<string_length; i++) {
-			var rnum = Math.floor(Math.random() * chars.length);
-			randomstring += chars.charAt(rnum);
-		}
-		return randomstring;
-	};
-	
-	//Here is where we can randomly generate the name of the styles to avoid them checking for specific names.
-	//Can add more here if they check additional names.
-	//ref:http://jonraasch.com/blog/javascript-style-node
-	var triArrowString = generator();
-	var css = document.createElement('style');
-	css.type = 'text/css';
-	var styles = '.'+triArrowString+'{ width: 0; height: 0; border-left: 9px solid transparent; border-right: 9px solid transparent;	border-top: 10px solid #494949; }';
-	styles += '.arrow:hover .'+triArrowString+'{ border-top: 10px solid #0063DC; }';
-	
-	//done this way for IE aparantly.
-	if (css.styleSheet) css.styleSheet.cssText = styles;
-	else css.appendChild(document.createTextNode(styles) );
-	
-	document.getElementsByTagName("head")[0].appendChild(css);
-	
-	
-	// Adds a download button next to each track
-	var buttonScript = function() {
-		console.debug("Buttons injected.");
-		// Wait for the tracks script to load
-		var tracks = window.displayList['tracks'];
-		 
-		if (tracks === undefined || tracks.length < 1) {
-			setTimeout(buttonScript, 1);
-		} 
-		else {
-				// Check if this particular page has been processed
-				// through a previous call
-			if (jQuery('.dl').length < tracks.length) {
-				jQuery('ul.tools').each(function(index, track) {
-					var song = tracks[index];
-					var artist = song.artist;          
-					var id = song.id;
-					var key = song.key;
-					// Sometimes the title is too long and is ellipsisisisisized in the JSON data
-					var title = jQuery("[data-itemid='" + id + "'] > .section-player > .track_name > a.track").attr('title').toString().split(" - ")[0];
+    //Random String generator.
+    var generator = function() {
+        var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+        var string_length = 8;
+        var randomstring = "";
+        for (var i = 0; i < string_length; i++) {
+            var rnum = Math.floor(Math.random() * chars.length);
+            randomstring += chars.charAt(rnum);
+        }
+        return randomstring;
+    };
 
-					console.log("index: "+index);
-					var hasDownloadButton = jQuery(track).data("hasDownloadButton");
-					if (typeof(hasDownloadButton) === 'undefined' || !hasDownloadButton){
-						jQuery.getJSON("/serve/source/"+ id + "/" + key, function(data) {
-						var download_url = data.url;
-						if (data.type != "SC")
-							jQuery(track).prepend('<li class="dl"><table class="spacer"></table><a target="_top" href="'+download_url+'"' + ' download="' + artist + ' - ' + title + '.mp3"' + '><table class="arrow"><tr><td><div class="rect-arrow"></div></td></tr><tr><td class="'+triArrowString+'"></td></tr></table></a></li>');
-						else 
-							jQuery(track).prepend('<li class="dl"><table class="spacer"></table><a target="_top" href="http://lander.in/id3/fix?url='+encodeURIComponent(download_url)+'&artist=' + encodeURIComponent(artist) + '&title=' + encodeURIComponent(title) + '"'+ '><table class="arrow"><tr><td><div class="rect-arrow"></div></td></tr><tr><td class="'+triArrowString+'"></td></tr></table></a></li>');
-						});
-						jQuery(track).data("hasDownloadButton", true);
-					}
-				});//each		
-			}
-		}
-	};//buttonscript
-  
-	
-	// Run it right away
-	buttonScript();
-  
-  jQuery(document).ajaxComplete(function(event,request, settings){
-		buttonScript();
-  });
-  
-	/*
+    //Here is where we can randomly generate the name of the styles to avoid them checking for specific names.
+    //Can add more here if they check additional names.
+    //ref:http://jonraasch.com/blog/javascript-style-node
+    var triArrowString = generator();
+    var css = document.createElement('style');
+    css.type = 'text/css';
+    var styles = '.' + triArrowString + '{ width: 0; height: 0; border-left: 9px solid transparent; border-right: 9px solid transparent;	border-top: 10px solid #494949; }';
+    styles += '.arrow:hover .' + triArrowString + '{ border-top: 10px solid #0063DC; }';
+
+    //done this way for IE aparantly.
+    if (css.styleSheet) css.styleSheet.cssText = styles;
+    else css.appendChild(document.createTextNode(styles));
+
+    document.getElementsByTagName("head")[0].appendChild(css);
+
+
+    // Adds a download button next to each track
+    var buttonScript = function() {
+        console.debug("Buttons injected.");
+        // Wait for the tracks script to load
+        var tracks = window.displayList['tracks'];
+
+        if (tracks === undefined || tracks.length < 1) {
+            setTimeout(buttonScript, 1);
+        } else {
+            // Check if this particular page has been processed
+            // through a previous call
+            if (jQuery('.dl').length < tracks.length) {
+                jQuery('ul.tools').each(function(index, track) {
+                    var song = tracks[index];
+                    var artist = song.artist;
+                    var id = song.id;
+                    var key = song.key;
+                    // Sometimes the title is too long and is ellipsisisisisized in the JSON data
+                    var title = jQuery("[data-itemid='" + id + "'] > .section-player > .track_name > a.track").attr('title').toString().split(" - ")[0];
+
+                    console.log("index: " + index);
+                    var hasDownloadButton = jQuery(track).data("hasDownloadButton");
+                    if (typeof(hasDownloadButton) === 'undefined' || !hasDownloadButton) {
+                        jQuery.getJSON("/serve/source/" + id + "/" + key, function(data) {
+                            var download_url = data.url;
+                            if (data.type != "SC")
+                                jQuery(track).prepend('<li class="dl"><table class="spacer"></table><a target="_top" href="' + download_url + '"' + ' download="' + artist + ' - ' + title + '.mp3"' + '><table class="arrow"><tr><td><div class="rect-arrow"></div></td></tr><tr><td class="' + triArrowString + '"></td></tr></table></a></li>');
+                            else
+                                jQuery(track).prepend('<li class="dl"><table class="spacer"></table><a target="_top" href="https://landaire.net/id3/fix?url=' + encodeURIComponent(download_url) + '&artist=' + encodeURIComponent(artist) + '&title=' + encodeURIComponent(title) + '"' + '><table class="arrow"><tr><td><div class="rect-arrow"></div></td></tr><tr><td class="' + triArrowString + '"></td></tr></table></a></li>');
+                        });
+                        jQuery(track).data("hasDownloadButton", true);
+                    }
+                }); //each		
+            }
+        }
+    }; //buttonscript
+
+
+    // Run it right away
+    buttonScript();
+
+    jQuery(document).ajaxComplete(function(event, request, settings) {
+        buttonScript();
+    });
+
+    /*
   The following code can be used to make the download attribute added in HTML5 work.
   HypeMachine overrides the handle_click functionality and returns false at the end, disabling the browsers default handling (i.e. downloading).
   In order to circumvent this, we can either set the target or added the check for our className. 
@@ -139,7 +137,7 @@ var main = function() {
 // Lets create the script objects
 var injectedScript = document.createElement('script');
 injectedScript.type = 'text/javascript';
-injectedScript.text = '('+main+')("");';
+injectedScript.text = '(' + main + ')("");';
 (document.body || document.head).appendChild(injectedScript);
 
 
